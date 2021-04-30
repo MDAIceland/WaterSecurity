a48f4f031f4c60a04aff34233c578097618b7958
Joachim Bache-Mathiesen <joachimbm96@gmail.com>
2021-04-30 13:50:17 +0200

    Reshape edstat to be countryXindicator

--- unlabeled_preprocessing/exploration/helpers.py
+++ unlabeled_preprocessing/exploration/helpers.py
@@ -1,3 +1,10 @@
 
-def dropColumnHalf(df,inplace=True):
-    df.dropna(thresh=len(df.index)/2, axis=1, inplace=inplace)
+def dropColumnHalf(df):
+    df.dropna(thresh=len(df.index)/2, axis=1, inplace=True)
+
+def fill_missing_with_column(df, into, fro):
+    """
+    Merges one column into the other filling null values of the into colun and removing the fro column
+    """
+    df[into] = df[into].combine_first(df[fro])
+    df.drop([fro], axis=1, inplace = True)
