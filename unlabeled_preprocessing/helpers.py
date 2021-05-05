15ccf0843811905b40ecfca2884493b7033392e7
Joachim Bache-Mathiesen <joachimbm96@gmail.com>
2021-05-05 07:27:17 +0200

    Export cleanup and check for all integer columns before imputation

--- unlabeled_preprocessing/helpers.py
+++ unlabeled_preprocessing/helpers.py
@@ -15,12 +15,13 @@ def fill_missing_with_column(df, into, fro):
     df[into] = df[into].combine_first(df[fro])
     df.drop([fro], axis=1, inplace = True)
 
-def unlabeled_df_imputer(df, max_iter=10, verbose=0):
+    
+def impute_df(df, max_iter=10, verbose=0):
     """
-    Imputes a df 
+    Imputes a df and returns a dataframe with the original and imputed values
     """
     imp = IterativeImputer(max_iter=max_iter, verbose=verbose)
-    imp.fit(df)
+    imp.fit_transform(df)
     imputed_df = imp.transform(df)
     return pd.DataFrame(imputed_df, columns=df.columns,index=df.index)
 
@@ -37,3 +38,11 @@ def print_missing_percentages(df):
     print("Min:", min_missing,'%')
     print("Mean:", mean_missing,'%')
     return min_missing, max_missing
+
+def find_all_integer_columns(df):
+    """
+    Returns an array of all colums that contain only integers or null values
+    """
+    integer_columns = df.applymap(lambda x: int(x)==x if pd.notnull(x) else x).prod().values.astype(bool)
+    return df.columns[integer_columns].values
+
