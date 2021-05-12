ff1c201fb866adbfb86ba4363941152d846ebc36
Joachim Bache-Mathiesen <joachimbm96@gmail.com>
2021-05-12 13:58:49 +0200

    Combines except econ

--- unlabeled_preprocessing/combine_unlabeled.py
+++ unlabeled_preprocessing/combine_unlabeled.py
@@ -5,5 +5,22 @@ sys.path.append("..")
 from data.unlabeled.preprocessed import econ, aqua, edu, humdev
 
 # %%
-econ
+print("Aqua shape:",aqua.shape,"Econ shape:",econ.shape,"Edu shape:",edu.shape,"Humdev shape:",humdev.shape)
 # %%
+countries_diff_edu_humdev= edu.loc[set(edu.index) - set(humdev.index)]['Short Name']
+countries_diff_aqua_humd = aqua.loc[set(aqua.index) - set(humdev.index)]['Country']
+countries_not_in_humdev = countries_diff_edu_humdev.append(countries_diff_aqua_humd)
+# %%
+print("Extra not needed indicators in edu dataset",countries_not_in_humdev)
+# %%
+countries_diff_humdev_econ = set(humdev.index) - set(econ.index) 
+print("Not in humdev",countries_diff_humdev_econ)
+
+# %%
+big_table = humdev.join(edu, how="inner").join(aqua, how="inner")
+# %%
+big_table.dropna(axis=1, inplace=True)
+# %%
+big_table.columns
+# %%
+big_table.to_csv("../data/unlabeled/preprocessed_unlabeled.csv")
