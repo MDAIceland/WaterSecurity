9bf580b25eba41ffad8c056978e7900ad2936dba
vaslem <vaslemonidisi@hotmail.com>
2021-05-18 00:19:20 +0200

    Created the unified dataset

--- data/unlabeled/__init__.py
+++ data/unlabeled/__init__.py
@@ -4,12 +4,12 @@ import pandas as pd
 UNLABELED_DIR = os.sep.join(os.path.split(__file__)[:-1])
 WORLD_CITIES_PATH = os.path.join(UNLABELED_DIR, "worldcities.csv")
 WORLD_CITIES = pd.read_csv(WORLD_CITIES_PATH)
+BIG_CITIES_PATH = os.path.join(UNLABELED_DIR, "bigcities.csv")
+BIG_CITIES_ALL_COUNTRIES_PATH = os.path.join(
+    UNLABELED_DIR, "bigcities_allcountries.csv"
+)
 try:
-    BIG_CITIES_PATH = os.path.join(UNLABELED_DIR, "big_cities.csv")
     BIG_CITIES = pd.read_csv(BIG_CITIES_PATH)
-    BIG_CITIES_ALL_COUNTRIES_PATH = os.path.join(
-        UNLABELED_DIR, "bigcities_all_countries.csv"
-    )
     BIG_CITIES_ALL_COUNTRIES = pd.read_csv(BIG_CITIES_ALL_COUNTRIES_PATH)
 except FileNotFoundError:
     pass
