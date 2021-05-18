ba63fb29d8c7cac26746fd16a60d72fd8ebfc487
Joachim Bache-Mathiesen <joachimbm96@gmail.com>
2021-05-18 14:46:21 +0200

    Working documentation

--- generate_documentation.py
+++ generate_documentation.py
@@ -7,13 +7,15 @@ modules = pdoc.Module(".", context=context, skip_errors=True)
 pdoc.link_inheritance(context)
 
 def recursive_htmls(mod):
-    yield mod.name, mod.html()
+    yield mod.name, mod.html(), bool(mod.submodules())
     for submod in mod.submodules():
         yield from recursive_htmls(submod)
 
-for module_name, html in recursive_htmls(modules):
-    fname = f"documentation/{'/'.join(module_name.split('.'))}/index.html"
+for module_name, html, has_subm in recursive_htmls(modules):
+    if has_subm:
+        fname = f"documentation/{'/'.join(module_name.split('.'))}/index.html"
+    else:
+        fname = f"documentation/{'/'.join(module_name.split('.'))}.html"
     os.makedirs(os.path.dirname(fname), exist_ok=True)
     with open(fname,"w", encoding="utf-8") as f:
         f.writelines(html)
-
