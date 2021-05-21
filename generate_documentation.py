12eb7eef99c821d7882ef0b890f4350038055f58
Joachim Bache-Mathiesen <joachimbm96@gmail.com>
2021-05-21 11:39:46 +0200

    Generate and link to notebooks

--- generate_documentation.py
+++ generate_documentation.py
@@ -1,5 +1,9 @@
+from nbconvert.exporters.exporter_locator import E
 import pdoc
 import os
+from nbconvert import HTMLExporter
+import nbformat
+
 context = pdoc.Context()
 
 with open(".docignore","r") as rf:
@@ -15,6 +19,7 @@ def recursive_htmls(mod):
         yield from recursive_htmls(submod)
 
 for module_name, html, has_subm in recursive_htmls(modules):
+    print(module_name)
     if has_subm:
         fname = f"documentation/{'/'.join(module_name.split('.'))}/index.html"
     else:
@@ -23,3 +28,14 @@ for module_name, html, has_subm in recursive_htmls(modules):
     with open(fname,"w", encoding="utf-8") as f:
         f.writelines(html)
 
+html_exporter = HTMLExporter()
+html_exporter.template_name = 'classic'
+
+for subdir, dirs, files in os.walk(r'.'):
+    for filename in files:
+        filepath = subdir + os.sep + filename
+        if filepath.endswith(".ipynb"):
+            with open(filepath, "r",encoding="utf-8") as fp:
+                (body, resources) = html_exporter.from_notebook_node(nbformat.read(fp, as_version=4))
+            with open("documentation/WaterSecurity/notebooks/"+os.path.basename(filepath).split(".")[0]+".html","w",encoding="utf-8") as exp:   
+                exp.writelines(body)
---
+++ unlabeled_preprocessing/__init__.py
@@ -0,0 +1,8 @@
+"""
+## Notebooks below
+* [Combining datasets](../notebooks/combine_unlabeled.html)
+* [Econ preprocessing](../notebooks/prep_economic_v2.html)
+* [Education preprocessing](../notebooks/prep_edstats.html)
+* [Human development preprocessing](../notebooks/prep_hdro_v2.html)
+* [Aquastat dataset](../notebooks/prep_aquastat.html)
+"""
