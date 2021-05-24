6cf1e65c9b7de8b7b844d9e708cb1998519cbe8b
Joachim Bache-Mathiesen <joachimbm96@gmail.com>
2021-05-24 19:35:50 +0200

    Documentation

---
+++ labeled_preprocessing/__init__.py
@@ -0,0 +1,12 @@
+"""
+## Labeled Preprocessing
+Here the initial data from [cdp.net](https://www.cdp.net/en/) regarding water security is preprocessed.
+The merging of the labeled and unlabeled dataset is also done here.
+
+## Notebooks
+* [Cities Test Set Processing](../notebooks/Cities Test Set Processing.html)
+* [Cities Population Density](../notebooks/CitiesPopulationDensity.html)
+* [Dataset Normalization and Imputation](../notebooks/Dataset Normalization and Imputation.html)
+* [Merge Unlabeled to Labeled](../notebooks/Merge Unlabeled to Labeled.html)
+* [Cities Test Set Processing](../notebooks/Cities Test Set Processing.html)
+"""
--- unlabeled_preprocessing/__init__.py
+++ unlabeled_preprocessing/__init__.py
@@ -1,5 +1,9 @@
 """
-## Notebooks below
+## Unlabeled Preprocessing
+Here, data from various sources are preprocessed to have as much useful data to feed into the model as possible.
+The helpers.py contains various helper functions for the preprocessing. The unlabeled data contains data on a per country.
+
+## Notebooks
 * [Combining datasets](../notebooks/combine_unlabeled.html)
 * [Econ preprocessing](../notebooks/prep_economic_v2.html)
 * [Education preprocessing](../notebooks/prep_edstats.html)
