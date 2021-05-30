0c10b26b0e974cc1dab2451387eaa86af2b807f0
Joachim Bache-Mathiesen <joachimbm96@gmail.com>
2021-05-30 19:59:31 +0200

    Changes to doc

--- water_security/__init__.py
+++ water_security/__init__.py
@@ -4,12 +4,15 @@ The current climate change scenario predicts that almost half of the world’s p
 
 Based on financial, educational, environmental and demographical data, this project aims to display and predict the water security risks around the world. In order to do so, a Regression Machine Learning pipeline is deployed per risk category (e.g. risk of higher water prices or risk of declining water quality), and a forecast of that risk's severity is made. The regression model is based on engineered and selected features from the data mentioned above.
 
-**In order to get the final dataset from the preprocessing the following notebooks have to be executed in the given order:**
+**The final dataset is already created, but if needed, can be generated again by running the following notebooks in the given order:**
 
-1. [prep_hdro_v2.ipynb](notebooks/prep_hdro_v2.html) 
+1. [prep_hdro_v2.ipynb](notebooks/prep_hdro_v2.html)
 2. [combine_unlabeled.ipynb](notebooks/combine_unlabeled.html)
 3. [Dataset Normalization and Imputation.ipynb](notebooks/Dataset Normalization and Imputation.html)
 4. [Cities Test Set Processing.ipynb](notebooks/Cities Test Set Processing.html)
 5. [Merge Unlabeled to Labeled.ipynb](notebooks/Merge Unlabeled to Labeled.html)
 6. [CitiesPopulationDensity.ipynb](notebooks/CitiesPopulationDensity.html)
+7. [Cities Elevation](notebooks/Cities Elevation.html)
+
+In order to generate a classification report run the [Classification Outcome Visualization.ipynb](notebooks/Classification Outcome Visualization.html) notebook.
 """
--- water_security/classification/__init__.py
+++ water_security/classification/__init__.py
@@ -5,6 +5,5 @@ The classification module contains classes for risk prediction.
 ## Notebooks
 * [Classification Outcome Visualization](../notebooks/Classification Outcome Visualization.html)
 * [XGBoost Fitting Using Bayesian Optimization](../notebooks/XGBoost Fitting Using Bayesian Optimization.html)
-* [feature selection](../notebooks/feature_selection.html)
 """
 RANDOM_SEED = 42
