d08b1dc954ed9480980e54bbf28388d13501be53
vaslem <vaslemonidisi@hotmail.com>
2021-05-30 22:06:17 +0200

    Added download status

--- water_security/utils/download.py
+++ water_security/utils/download.py
@@ -1,4 +1,7 @@
+import threading
 import urllib
+from typing import Tuple
+
 from utils.notebook import isnotebook
 
 if isnotebook:
@@ -6,41 +9,68 @@ if isnotebook:
 else:
     from tqdm import tqdm
 
+from collections import deque
+
 
 class DownloadProgressBar(tqdm):
     """
     Progress Bar for downloading purposes
     """
 
+    def __init__(self, *args, download_queue=None, **kwargs):
+        self.download_queue = download_queue
+        self.cnt = 0
+        super().__init__(self, *args, **kwargs)
+
     def update_to(self, b=1, bsize=1, tsize=None):
+        global DOWNLOAD_PROGRESS_
         if tsize is not None:
             self.total = tsize
+
         self.update(b * bsize - self.n)
+        self.cnt += bsize
+        if self.download_queue is not None:
+            self.download_queue.append(
+                f"{round(100*self.cnt/1024**2)/100}MB/{round(100*self.total/1024**2)/100}MB"
+            )
 
 
-def download_url_while_in_notebook(url, output_path):
-    import threading
+def download_url_while_in_notebook(url, output_path) -> Tuple[threading.Thread, deque]:
+    """
+    Returns download thread and list that contains download progress string
+    """
     from IPython.display import display
 
+    queue = deque(maxlen=10)
     progress = DownloadProgressBar(
-        unit="B", unit_scale=True, miniters=1, desc=url.split("/")[-1]
+        unit="B",
+        unit_scale=True,
+        miniters=1,
+        desc=url.split("/")[-1],
+        download_queue=queue,
     )
-
     thread = threading.Thread(
-        target=download_url_while_not_in_notebook, args=(url, output_path, progress)
+        target=download_url_while_not_in_notebook,
+        args=(url, output_path, progress),
     )
-    display(progress)
     thread.start()
-    return thread
+    display(progress)
+    return thread, queue
 
 
-def download_url_while_not_in_notebook(url: str, output_path: str, widget=None) -> None:
+def download_url_while_not_in_notebook(
+    url: str, output_path: str, widget=None, queue=None
+) -> None:
     """
     Download a file from a url and save it to the provided output path
     """
     if widget is None:
         widget = DownloadProgressBar(
-            unit="B", unit_scale=True, miniters=1, desc=url.split("/")[-1]
+            unit="B",
+            unit_scale=True,
+            miniters=1,
+            desc=url.split("/")[-1],
+            download_queue=queue,
         )
     with widget as t:
         urllib.request.urlretrieve(url, filename=output_path, reporthook=t.update_to)
